6c7fb0321b6154a79c1eff56aa4852f31c0a4714
hstrejoluna <trejolunatutoriales@gmail.com>
2021-09-27 12:44:18 -0500

    styled components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "./components/Header";
 
-function App() {
-  return (
-    <Header>
-      title="Car Insurance Quote"
-    </Header>
-  );
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContainerForm = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+import styled from "@emotion/styled";
+
+const App = () => {
+  return <Header title="Car Insurance Quote" />;
 }
 
 export default App;
